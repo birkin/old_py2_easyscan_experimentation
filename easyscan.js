dc29b89fd5c6c54766c7e629f816b9b7c7a7d945
birkin <birkin.diana@gmail.com>
2014-11-18 12:02:00 -0500

    fields row data.

--- easyscan.js
+++ easyscan.js
@@ -5,21 +5,39 @@ console.log( "- easyscan.js START" );
 rows = $( ".bibItemsEntry" );
 console.log( "- rows.length, " + rows.length );
 
-row = rows[0];
-cells = row.getElementsByTagName("td");
-console.log( "- cells.length, " + cells.length );
+for (var i = 0; i < rows.length; i++) {
+  row = rows[i];
+  row_dict = extract_row_data( row );
+  console.log( "- row_dict, " + JSON.stringify(row_dict, null, 4) );
+}
 
-cell1 = cells[0]
-// console.log( "- cell, " + JSON.stringify(cell, null, 4) );  // circular error
-console.log( "- cell1 textContent, " + cell1.textContent );
-
-cell2 = cells[1]
-console.log( "- cell2 textContent, " + cell2.textContent );
-
-cell3 = cells[2]
-console.log( "- cell3 textContent, " + cell3.textContent );
-
+function extract_row_data( row ) {
+  cells = row.getElementsByTagName("td");
+  var row_data = {
+    "location": cells[0].textContent.trim(),
+    "call_number": cells[1].textContent.trim(),
+    "barcode": cells[2].textContent.trim(),
+    "availability": cells[3].textContent.trim()
+  };
+  return row_data;
+}
 
 console.log( "- easyscan.js END" );
 
-// JSON.stringify(obj)
+// JSON.stringify(obj, null, 4)
+
+
+
+// row = rows[0];
+// cells = row.getElementsByTagName("td");
+// console.log( "- cells.length, " + cells.length );
+
+// cell1 = cells[0]
+// // console.log( "- cell, " + JSON.stringify(cell, null, 4) );  // circular error
+// console.log( "- cell1 textContent, " + cell1.textContent );
+
+// cell2 = cells[1]
+// console.log( "- cell2 textContent, " + cell2.textContent );
+
+// cell3 = cells[2]
+// console.log( "- cell3 textContent, " + cell3.textContent );
