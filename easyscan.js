6b2e54f94413eb953a73a586ae2042a4a6516e22
birkin <birkin.diana@gmail.com>
2014-11-18 12:31:06 -0500

    adds initial link.

--- easyscan.js
+++ easyscan.js
@@ -8,7 +8,10 @@ console.log( "- rows.length, " + rows.length );
 for (var i = 0; i < rows.length; i++) {
   row = rows[i];
   row_dict = extract_row_data( row );
-  console.log( "- row_dict, " + JSON.stringify(row_dict, null, 4) );
+  link_html = build_link_html( row_dict )
+  last_cell = row.getElementsByTagName("td")[3];
+  // $( last_cell ).after( "<td>HELLO</td>" );
+  $( last_cell ).after( link_html );
 }
 
 function extract_row_data( row ) {
@@ -19,25 +22,19 @@ function extract_row_data( row ) {
     "barcode": cells[2].textContent.trim(),
     "availability": cells[3].textContent.trim()
   };
+  console.log( "- row_data, " + JSON.stringify(row_data, null, 4) );
   return row_data;
+}
+
+function build_link_html( row_dict ) {
+  // link = '<a href="http://google.com">LINK</a>';
+  link = '<a href="http://127.0.0.1/easyscan/request?call_number=THECALLNUMBER&barcode=THEBARCODE">Request Scan</a>';
+  link = link.replace( "THECALLNUMBER", row_dict["call_number"] );
+  link = link.replace( "THEBARCODE", row_dict["barcode"] );
+  console.log( "- link end, " + link );
+  return link;
 }
 
 console.log( "- easyscan.js END" );
 
 // JSON.stringify(obj, null, 4)
-
-
-
-// row = rows[0];
-// cells = row.getElementsByTagName("td");
-// console.log( "- cells.length, " + cells.length );
-
-// cell1 = cells[0]
-// // console.log( "- cell, " + JSON.stringify(cell, null, 4) );  // circular error
-// console.log( "- cell1 textContent, " + cell1.textContent );
-
-// cell2 = cells[1]
-// console.log( "- cell2 textContent, " + cell2.textContent );
-
-// cell3 = cells[2]
-// console.log( "- cell3 textContent, " + cell3.textContent );
