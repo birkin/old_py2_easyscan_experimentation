9836729e4588cd52e7f64cf72e4ef26f5a642877
birkin <birkin.diana@gmail.com>
2014-11-18 11:28:55 -0500

    captures relevant cells.

--- easyscan.js
+++ easyscan.js
@@ -1,7 +1,25 @@
-alert( "hi_there" );
+console.log( "- easyscan.js START" );
+
 
 // jquery already loaded (whew)
 rows = $( ".bibItemsEntry" );
-alert( rows.length );
+console.log( "- rows.length, " + rows.length );
 
-alert( "bye now" );
+row = rows[0];
+cells = row.getElementsByTagName("td");
+console.log( "- cells.length, " + cells.length );
+
+cell1 = cells[0]
+// console.log( "- cell, " + JSON.stringify(cell, null, 4) );  // circular error
+console.log( "- cell1 textContent, " + cell1.textContent );
+
+cell2 = cells[1]
+console.log( "- cell2 textContent, " + cell2.textContent );
+
+cell3 = cells[2]
+console.log( "- cell3 textContent, " + cell3.textContent );
+
+
+console.log( "- easyscan.js END" );
+
+// JSON.stringify(obj)
