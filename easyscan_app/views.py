cfdcc3ffaff1ab38847d14a0181fd10df141e8bb
birkin <birkin.diana@gmail.com>
2014-11-21 15:25:45 -0500

    neatens.

--- easyscan_app/views.py
+++ easyscan_app/views.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home( request ):
   title = request.GET.get( 'title', u'' )
   callnumber = request.GET.get( 'call_number', u'' )
   barcode = request.GET.get( 'barcode', u'' )
-  resp = u'''
-    <p>Thanks for testing!</p>
-    <p>The item info that would be processed...</p>
-    <ul>
-      <li>Title: `%s`</li>
-      <li>Callnumber, etc info: `%s`</li>
-      <li>Barcode: `%s`</li>
-    </ul>
-    ''' % ( title, callnumber, barcode )
+  html = temp_html( title, callnumber, barcode )
+  return HttpResponse( html )
 
-  return HttpResponse( resp )
+
+def temp_html( title, callnumber, barcode ):
+    html = u'''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>easyscan data check</title>
+    </head>
+    <body>
+        <p>Thanks for testing!</p>
+        <p>The item info that would be processed...</p>
+        <ul>
+            <li>title: `%s`</li>
+            <li>call-number, etc info: `%s`</li>
+            <li>barcode: `%s`</li>
+        </ul>
+    </body>
+    </html>''' % ( title, callnumber, barcode )
+    return html
