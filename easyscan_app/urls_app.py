95c68b816fd46e9d3956bde00c4507567a726bd5
birkin <birkin.diana@gmail.com>
2014-11-21 15:15:55 -0500

    shows basic input.

--- easyscan_app/urls_app.py
+++ easyscan_app/urls_app.py
@@ -6,7 +6,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
 
-    url( r'^home/$',  'easyscan_app.views.home', name=u'home_url' ),
+    url( r'^request/$',  'easyscan_app.views.home', name=u'home_url' ),
 
     url( r'^$', RedirectView.as_view(pattern_name='home_url') ),
 
--- easyscan_app/views.py
+++ easyscan_app/views.py
@@ -5,4 +5,17 @@ from django.shortcuts import render
 
 
 def home( request ):
-  return HttpResponse( u'<p>hello world</p>' )
+  title = request.GET.get( 'title', u'' )
+  callnumber = request.GET.get( 'call_number', u'' )
+  barcode = request.GET.get( 'barcode', u'' )
+  resp = u'''
+    <p>Thanks for testing!</p>
+    <p>The item info that would be processed...</p>
+    <ul>
+      <li>Title: `%s`</li>
+      <li>Callnumber, etc info: `%s`</li>
+      <li>Barcode: `%s`</li>
+    </ul>
+    ''' % ( title, callnumber, barcode )
+
+  return HttpResponse( resp )
