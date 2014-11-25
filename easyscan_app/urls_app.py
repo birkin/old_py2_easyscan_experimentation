eb5aef2746992927fd537f7faa12e076a871c4a7
birkin <birkin.diana@gmail.com>
2014-11-25 15:48:43 -0500

    names view more logically.

--- easyscan_app/urls_app.py
+++ easyscan_app/urls_app.py
@@ -6,12 +6,12 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
 
-    url( r'^request/$',  'easyscan_app.views.home', name=u'home_url' ),
+    url( r'^request/$',  'easyscan_app.views.request_def', name=u'request_url' ),
 
     url( r'^josiah_easyscan.js/$',  'easyscan_app.views.js', name=u'js_url' ),
 
     url( r'^login/$',  'easyscan_app.views.login', name=u'login_url' ),
 
-    url( r'^$', RedirectView.as_view(pattern_name='home_url') ),
+    url( r'^$', RedirectView.as_view(pattern_name='request_url') ),
 
     )
--- easyscan_app/views.py
+++ easyscan_app/views.py
@@ -13,12 +13,12 @@ def js( request ):
     js_path = u'%s/lib/josiah_easyscan.js' % current_directory
     with open( js_path ) as f:
         js_utf8 = f.read()
-        js_unicode = js_utf8.decode(u'utf-8')
+        js_unicode = js_utf8.decode( u'utf-8' )
     js_unicode = js_unicode.replace( u'HOST', request.get_host() )
     return HttpResponse( js_unicode, content_type = u'application/javascript; charset=utf-8' )
 
 
-def home( request ):
+def request_def( request ):
     data_dict = {
         'title': request.GET.get( 'title', u'' ),
         'callnumber': request.GET.get( 'call_number', u'' ),
@@ -29,35 +29,3 @@ def home( request ):
 
 def login( request ):
     return HttpResponse( u'<p>patience, padawan, you must have</p>' )
-
-
-# def home( request ):
-#     title = request.GET.get( 'title', u'' )
-#     callnumber = request.GET.get( 'call_number', u'' )
-#     barcode = request.GET.get( 'barcode', u'' )
-#     html = temp_html( title, callnumber, barcode )
-#     # return HttpResponse( html )
-#     data_dict = {}
-#     data_dict[u'foo'] = "bar"
-#     return render( request, u'easyscan_app_templates/request.html', data_dict )
-
-
-def temp_html( title, callnumber, barcode ):
-    html = u'''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>easyscan data check</title>
-    </head>
-    <body>
-        <p>Thanks for testing!</p>
-        <p>The item info that would be processed...</p>
-        <ul>
-            <li>title: `%s`</li>
-            <li>call-number, etc info: `%s`</li>
-            <li>barcode: `%s`</li>
-        </ul>
-    </body>
-    </html>''' % ( title, callnumber, barcode )
-    return html
