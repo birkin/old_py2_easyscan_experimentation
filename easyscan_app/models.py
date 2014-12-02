c30a1c2d842183ec7868c407a3da0feb20a465aa
birkin <birkin.diana@gmail.com>
2014-12-02 17:11:24 -0500

    calls api for name/barcode auth.

--- easyscan_app/models.py
+++ easyscan_app/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import logging
+import logging, os
+import requests
 from django.db import models
 
 
@@ -15,7 +16,7 @@ class RequestValidator( object ):
         """ Checks for https; returns dict with result and redirect-url.
             Called by views.request_def() """
         if (is_secure == False) and (get_host != u'127.0.0.1'):
-            redirect_url = redirect_url = u'https://%s%s' % ( get_host, full_path )
+            redirect_url = u'https://%s%s' % ( get_host, full_path )
             return_dict = { u'is_secure': False, u'redirect_url': redirect_url }
         else:
             return_dict = { u'is_secure': True, u'redirect_url': u'N/A' }
@@ -37,3 +38,68 @@ class RequestPageHelper( object ):
             }
         log.debug( u'in RequestPageHelper.build_data_dict(); return_dict, `%s`' % context )
         return context
+
+
+class BarcodeValidator( object ):
+    """ Container for helpers checking submitted patron barcode & name.
+        Non-django, plain-python model. """
+
+    def __init__( self ):
+        self.api_root_url = os.environ.get(u'EZSCAN__PATRONAPI_ROOT_URL', u'')
+
+    def check_barcode( self, barcode, name ):
+        """ Controller function: calls request, parse, and evaluate functions. """
+        raw_data = self.grab_raw_data( barcode )
+        log.debug( u'in BarcodeValidator.check_barcode(); raw_data, `%s`' % raw_data )
+        if (u'403 Forbidden' in raw_data) or raw_data.startswith( u'Exception' ):
+            return { u'validity': u'invalid', u'error': raw_data }
+        parsed_data = self.parse_raw_data( raw_data )
+        evaluation_dict = self.evaluate_parsed_data( parsed_data, name )
+        return evaluation_dict
+
+    def grab_raw_data( self, barcode ):
+        try:
+            url = u'%s/%s/dump' % ( self.api_root_url, barcode )
+            r = requests.get( url )
+            raw_data = r.content.decode( u'utf-8' )
+        except Exception as e:
+            raw_data = u'Exception, `%s`' % unicode(repr(e))
+        return raw_data
+
+    def parse_raw_data( self, raw_data ):
+        lines = raw_data.split( u'\n' )
+        parsed_data = {}
+        for line in lines:
+            if u'PATRN NAME' in line:
+                parsed_data[u'name'] = line
+            if u'E-MAIL' in line:
+                parsed_data[u'email'] = line
+        parsed_data[u'name'] = self.parse_name( parsed_data[u'name'] )
+        parsed_data[u'email'] = self.parse_email( parsed_data[u'email'] )
+        log.debug( u'in BarcodeValidator.parse_raw_data(); parsed_data, `%s`' % parsed_data )
+        return parsed_data
+
+    def parse_name( self, name_line ):
+        start_position = len( u'PATRN NAME[pn]=' )
+        end_position = name_line.find( u'<BR>' )
+        name = name_line[start_position:end_position]
+        return name
+
+    def parse_email( self, email_line ):
+        start_position = len( u'E-MAIL[pe]=' )
+        end_position = email_line.find( u'<BR>' )
+        email = email_line[start_position:end_position].lower()
+        return email
+
+    def evaluate_parsed_data( self, parsed_data, name ):
+        all_parts = []
+        last_first_elements = parsed_data[u'name'].split( u',' )  # 'last, first middle' becomes ['last', 'first middle']
+        for element in last_first_elements:
+            split_parts = element.strip().split()
+            for part in split_parts:
+                all_parts.append( part.lower() )  # all_parts becomes ['last', 'first', 'middle']
+        if name.lower() in all_parts:
+            evaluation_dict = { u'validity': u'valid', u'name': name, u'email': parsed_data[u'email'] }
+        else:
+            evaluation_dict = { u'validity': u'invalid' }
+        return evaluation_dict
--- easyscan_app/views.py
+++ easyscan_app/views.py
@@ -8,8 +8,9 @@ from easyscan_app import models
 
 
 log = logging.getLogger(__name__)
-rv = models.RequestValidator()
+request_validator = models.RequestValidator()
 request_page_helper = models.RequestPageHelper()
+barcode_validator = models.BarcodeValidator()
 
 
 def js( request ):
@@ -27,7 +28,7 @@ def js( request ):
 
 def request_def( request ):
     """ Either displays login buttons, or a form to specify requested scan-content. """
-    https_check = rv.check_https(
+    https_check = request_validator.check_https(
         request.is_secure(), request.get_host(), request.get_full_path() )
     if https_check[u'is_secure'] == False:
         return HttpResponseRedirect( https_check[u'redirect_url'] )
@@ -50,10 +51,19 @@ def barcode_login( request ):
         Redirects to request form on success. """
     log.debug( u'in barcode_login()' )
     if request.method == u'POST':
+        barcode_check = u'init'
+        barcode_check = barcode_validator.check_barcode(
+            request.POST.get(u'barcode', u''), request.POST.get(u'name', u'') )
+        log.debug( u'in barcode_login(); barcode_check, `%s`' % barcode_check )
+        if barcode_check[u'validity'] == u'valid':
+            request.session[u'authz_info'][u'authorized'] = True
+            request.session[u'user_info'] = { u'name': barcode_check[u'name'], u'email':barcode_check[u'email'] }
+            redirect_url = u'https://%s%s' % ( request.get_host(), reverse(u'request_url') )
+            return HttpResponseRedirect( redirect_url )
         return HttpResponse( u'submitted data will be handled here.' )
-    data_dict = {
-        u'title': request.GET.get( u'title', u'' ),
-        u'callnumber': request.GET.get( u'callnumber', u'' ),
-        u'barcode': request.GET.get( u'barcode', u'' )
-        }
-    return render( request, u'easyscan_app_templates/barcode_login.html', data_dict )
+    else:
+        data_dict = {
+            u'title': request.GET.get( u'title', u'' ),
+            u'callnumber': request.GET.get( u'callnumber', u'' ),
+            u'barcode': request.GET.get( u'barcode', u'' ) }
+        return render( request, u'easyscan_app_templates/barcode_login.html', data_dict )
