4f1c5a9c994f65d141cb8cb136240a3088c19270
birkin <birkin.diana@gmail.com>
2014-12-09 09:54:49 -0500

    cleans up views.

--- easyscan_app/views.py
+++ easyscan_app/views.py
@@ -14,7 +14,7 @@ barcode_view_helper = models.BarcodeViewHelper()
 
 def js( request ):
     """ Returns javascript file.
-        May switch to direct apache serving, but this allows the 'Request Scan' link to be set dynamically, useful for testing. """
+        Will switch to direct apache serving, but this allows the 'Request Scan' link to be set dynamically, useful for testing. """
     js_unicode = u''
     current_directory = os.path.dirname(os.path.abspath(__file__))
     js_path = u'%s/lib/josiah_easyscan.js' % current_directory
@@ -26,7 +26,8 @@ def js( request ):
 
 
 def request_def( request ):
-    """ Either displays login buttons, or a form to specify requested scan-content. """
+    """ On GET, redirects to login options, or displays form to specify requested scan-content.
+        On POST, saves data and redirects to confirmation page. """
     if request.method == u'GET':
         return_response = request_view_helper.handle_get( request )
         return return_response
@@ -38,46 +39,33 @@ def request_def( request ):
         return  HttpResponseRedirect( redirect_url )
 
 
-# def request_def( request ):
-#     """ Either displays login buttons, or a form to specify requested scan-content. """
-#     if request.method == u'GET':
-#         return_response = request_view_helper.handle_get( request )
-#         return return_response
-#     else:  # POST of form
-#         ## save data here
-#         try:
-#             scnrqst = models.ScanRequest()
-#             scnrqst.item_title = request.session[u'item_info'][u'title']
-#             scnrqst.item_barcode = request.session[u'item_info'][u'barcode']
-#             scnrqst.item_callnumber = request.session[u'item_info'][u'callnumber']
-#             scnrqst.item_custom_info = request.POST.get( u'custom_info'.strip(), u'' )
-#             scnrqst.patron_name = request.session[u'user_info'][u'name']
-#             scnrqst.patron_barcode = request.session[u'user_info'][u'patron_barcode']
-#             scnrqst.patron_email = request.session[u'user_info'][u'email']
-#             scnrqst.save()
-#         except Exception as e:
-#             log.debug( u'in request_def(); exception, `%s`' % unicode(repr(e)) )
-#         request.session[u'authz_info'][u'authorized'] = False
-#         scheme = u'https' if request.is_secure() else u'http'
-#         redirect_url = u'%s://%s%s' % ( scheme, request.get_host(), reverse(u'confirmation_url') )
-#         return  HttpResponseRedirect( redirect_url )
-
-
 def shib_login( request ):
     log.debug( u'in shib_login()' )
     return HttpResponse( u'will handle shib-login; barcode-login has been implemented, try that for now' )
 
 
 def barcode_login( request ):
-    """ Displays barcode login form.
-        Redirects to request form on success. """
-    if request.method == u'POST':
-        return_response = barcode_view_helper.handle_post( request )
-        return return_response
-    else:
+    """ On GET, displays barcode login form.
+        On POST, redirects to request form on success, or barcode login form again on fail. """
+    if request.method == u'GET':
         data_dict = barcode_view_helper.build_data_dict( request )
         log.debug( u'in barcode_login(); data_dict, `%s`' % pprint.pformat(data_dict) )
         return render( request, u'easyscan_app_templates/barcode_login.html', data_dict )
+    else:  # POST of form
+        return_response = barcode_view_helper.handle_post( request )
+        return return_response
+
+
+# def barcode_login( request ):
+#     """ On GET, displays barcode login form.
+#         On POST, redirects to request form on success, or barcode login form again on fail. """
+#     if request.method == u'POST':
+#         return_response = barcode_view_helper.handle_post( request )
+#         return return_response
+#     else:
+#         data_dict = barcode_view_helper.build_data_dict( request )
+#         log.debug( u'in barcode_login(); data_dict, `%s`' % pprint.pformat(data_dict) )
+#         return render( request, u'easyscan_app_templates/barcode_login.html', data_dict )
 
 
 def confirmation( request ):
