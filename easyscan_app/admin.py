b62e54b766e0d5d1d0d9ff4e31adc7ec8aef7615
birkin <birkin.diana@gmail.com>
2015-02-05 11:26:55 -0500

    adds additional form field.

--- easyscan_app/admin.py
+++ easyscan_app/admin.py
@@ -9,18 +9,18 @@ class ScanRequestAdmin( admin.ModelAdmin ):
     ordering = [ u'id' ]
     list_display = [
         u'id', u'create_datetime',
-        u'item_title', u'item_barcode', u'item_callnumber', u'item_volume_year', u'item_chap_vol_title', u'item_source_url', u'item_page_range_other',
+        u'item_title', u'item_barcode', u'item_callnumber', u'item_volume_year', u'item_chap_vol_title', u'item_source_url', u'item_page_range_other', u'item_other',
         u'patron_name', u'patron_barcode', u'patron_email',
         u'las_conversion' ]
     # list_filter = [ u'patron_barcode' ]
     search_fields = [
         u'id', u'create_datetime',
-        u'item_title', u'item_barcode', u'item_callnumber', u'item_volume_year', u'item_chap_vol_title', u'item_source_url', u'item_page_range_other',
+        u'item_title', u'item_barcode', u'item_callnumber', u'item_volume_year', u'item_chap_vol_title', u'item_source_url', u'item_page_range_other', u'item_other',
         u'patron_name', u'patron_barcode', u'patron_email',
         u'las_conversion' ]
     readonly_fields = [
         u'id', u'create_datetime',
-        u'item_title', u'item_barcode', u'item_callnumber', u'item_volume_year', u'item_chap_vol_title', u'item_source_url', u'item_page_range_other',
+        u'item_title', u'item_barcode', u'item_callnumber', u'item_volume_year', u'item_chap_vol_title', u'item_source_url', u'item_page_range_other', u'item_other',
         u'patron_name', u'patron_barcode', u'patron_email',
         u'las_conversion' ]
 
--- easyscan_app/easyscan_forms.py
+++ easyscan_app/easyscan_forms.py
@@ -4,10 +4,12 @@ from django import forms
 
 
 class CitationForm( forms.Form ):
-    # article_chapter_title = forms.CharField( label=u'Article/Chapter title...', max_length=500 )
     article_chapter_title = forms.CharField(
-        label=u'Article/Chapter title...', max_length=500, widget=forms.Textarea( attrs={'rows':2, 'cols':50} )
+        label=u'Article/Chapter title (required)', max_length=500, widget=forms.Textarea( attrs={'rows':2, 'cols':50} )
         )
     page_range = forms.CharField(
-        label=u'Page range...', max_length=500, widget=forms.Textarea( attrs={'rows':2, 'cols':50} )
+        label=u'Page range (required)', max_length=500, widget=forms.Textarea( attrs={'rows':2, 'cols':50} )
+        )
+    other = forms.CharField(
+        label=u'Other (optional)', required=False, max_length=500, widget=forms.Textarea( attrs={'rows':2, 'cols':50} )
         )
