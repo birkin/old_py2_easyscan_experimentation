3bacff50fc447dbe4f2824b1de765e0b7176a06b
birkin <birkin.diana@gmail.com>
2015-03-09 09:55:35 -0400

    sets js urls to library.brown.edu.

--- easyscan_app/urls_app.py
+++ easyscan_app/urls_app.py
@@ -10,14 +10,13 @@ urlpatterns = patterns('',
 
     url( r'^info/$',  'easyscan_app.views.info', name=u'info_url' ),
 
-    url( r'^josiah_easyscan.js/$',  'easyscan_app.views.easyscan_js', name=u'easyscan_js_url' ),  # this allows easy development, but in future, serve via apache directly
-    url( r'^josiah_request_item.js/$',  'easyscan_app.views.request_item_js', name=u'request_item_js_url' ),  # this allows easy development, but in future, serve via apache directly
-
     url( r'^shib_login/$',  'easyscan_app.views.shib_login', name=u'shib_login_url' ),
-    # url( r'^barcode_login/$',  'easyscan_app.views.barcode_login', name=u'barcode_login_url' ),  # 2012-Dec: no longer needed
     url( r'^logout/$',  'easyscan_app.views.shib_logout', name=u'logout_url' ),
 
     url( r'^confirmation/$',  'easyscan_app.views.confirmation', name=u'confirmation_url' ),
+
+    url( r'^dev_josiah_easyscan.js/$',  'easyscan_app.views.easyscan_js', name=u'easyscan_js_url' ),  # replaces hardcoded urls for easy local development
+    url( r'^dev_josiah_request_item.js/$',  'easyscan_app.views.request_item_js', name=u'request_item_js_url' ),  # replaces hardcoded urls for easy local development
 
     url( r'^$',  RedirectView.as_view(pattern_name=u'info_url') ),
 
