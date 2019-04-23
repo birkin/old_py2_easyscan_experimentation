884e57d50089f6b3ef15d10325cc6a375f386590
birkin <birkin.diana@gmail.com>
2019-04-23 16:09:58 -0400

    cleans up wsgi config file.

--- easyscan_config/passenger_wsgi.py
+++ easyscan_config/passenger_wsgi.py
@@ -1,64 +1,6 @@
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-# from __future__ import unicode_literals
-
-# """
-# Prepares application environment.
-
-# It exposes the WSGI callable as a module-level variable named ``application``.
-
-# Variables assume project setup like:
-# stuff
-#     easyaccess_project
-#         config
-#         delivery
-#         findit
-#     env_eza
-# """
-
-# import os, pprint, sys
-
-
-# ## become self-aware, padawan
-# current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# ## vars
-# ACTIVATE_FILE = os.path.abspath( u'%s/../../env_ezscan/bin/activate_this.py' % current_directory )
-# PROJECT_DIR = os.path.abspath( u'%s/../../easyscan_project' % current_directory )
-# PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
-# SETTINGS_MODULE = u'easyscan_config.settings'
-# SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_ezscan/lib/python2.7/site-packages' % current_directory )
-
-# ## virtualenv
-# execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
-
-# ## sys.path additions
-# for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
-#  if entry not in sys.path:
-#    sys.path.append( entry )
-
-# ## environment additions
-# os.environ[u'DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
-
-# ## load up env vars
-# SETTINGS_FILE = os.environ['EZSCAN__ENV_SETTINGS_PATH']  # set in activate_this.py, and activated above
-# import shellvars
-# var_dct = shellvars.get_vars( SETTINGS_FILE )
-# for ( key, val ) in var_dct.items():
-#     os.environ[key] = val
-
-# ## gogogo
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
-
-
-
-
-
-
-
-
+from __future__ import unicode_literals
 
 """
 WSGI config.
@@ -100,4 +42,3 @@ for ( key, val ) in var_dct.items():
 ## gogogo
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
